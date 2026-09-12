b973b193e2f40585f95676c204390f2692125abd
melon <132408878+meloncholic@users.noreply.github.com>
2026-09-11 21:54:25 -0400

    fix: advance grammar packages to patch version 0.1.1

--- dataweave/src/parser.c
+++ dataweave/src/parser.c
@@ -79595,7 +79595,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_dataweave(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
--- raml/src/parser.c
+++ raml/src/parser.c
@@ -41382,7 +41382,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_raml(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
